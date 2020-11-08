bfba9b289c341cae86fddd52aadfbec2b6ed1ac3
Michele Saba <pymike00@gmail.com>
2020-11-08 21:15:06 +0100

    add PScan Class and threadpool_executer method

--- src/p_scan.py
+++ src/p_scan.py
@@ -1,27 +1,32 @@
+from multiprocessing.pool import ThreadPool
+import os
 import socket
 
-OPEN_PORTS = list()
+class PScan:
 
+    def __init__(self):
+        self.open_ports = list()
+        self.remote_host = ""
 
-def scan_port(remote_host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn_status = sock.connect_ex((remote_host, port))
-    if conn_status == 0:
-        print(f"\nPort {port} is OPEN\n")
-        OPEN_PORTS.append(port)
-    sock.close()
+    def scan_port(self, port):
+        print(f"Scanning Port {port}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn_status = sock.connect_ex((self.remote_host, port))
+        if conn_status == 0:
+            print(f"\nPort {port} is OPEN\n")
+            self.open_ports.append(port)
+        sock.close()
 
+    def threadpool_executer(self, ports):
+        number_of_workers = os.cpu_count()
+        with ThreadPool(number_of_workers) as pool:
+            pool.map(self.scan_port, ports)
 
-def run():
-    remote_host = input("Target: ")
-    for port in range(1, 81):
-        print(f"Scanning port: {port}")
-        scan_port(remote_host, port)
+    def run(self):
+        self.remote_host = input("Target: ")
+        self.threadpool_executer(range(1, 1000))
 
 
 if __name__ == "__main__":
-    run()
-    if OPEN_PORTS:
-        print("#" * 20)
-        print("Open Ports:")
-        print(*OPEN_PORTS, sep=', ')
+    scanner = PScan()
+    scanner.run()
