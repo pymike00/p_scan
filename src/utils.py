a64e3bcf99cb9a472cc4937455d1a100fe3d6632
Michele Saba <pymike00@gmail.com>
2020-11-09 16:42:03 +0100

    add get_ports function to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
 import json
 
+PORTS_DATA_FILE = "./common_ports.json"
+
+
 def extract_json_data(filename):
     with open(filename, "r") as file:
         data = json.load(file)
     return data
+
+
+def get_ports():
+    data = extract_json_data(PORTS_DATA_FILE)
+    return {int(k): v for (k, v) in data.items()}
+
+
+if __name__ == "__main__":
+    print(get_ports())
